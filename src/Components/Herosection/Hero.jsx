4e9e0fd6f4ec2ea1315d291d9b67f1d111f0c7c6
Malik-Sameer <phullarsameer@gmail.com>
2025-03-04 19:18:11 +0500

    Adding missing files

--- src/Components/Herosection/Hero.jsx
+++ src/Components/Herosection/Hero.jsx
@@ -19,11 +19,16 @@ import {
   DiReact,
 } from "react-icons/di";
 import { motion } from "framer-motion";
-import { Link } from "react-scroll";
-
-
 
 const Hero = () => {
+    const handleDownload = () => {
+        const link = document.createElement("a");
+        link.href = "/Sameer_cv.pdf"; // File in the public folder
+        link.download = "CV.pdf"; // Suggested filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
   return (
     <div className="mt-24 max-w-[1200px] mx-auto relative">
         <div className="grid md:grid-cols-2 place-items-center gap-8">
@@ -75,13 +80,14 @@ const Hero = () => {
                 transition={{ duration: 1, delay: 1.5 }}
                 className="flex flex-row items-center gap-6 my-4 md:mb-0"
                 >
-                    <a href="../../assets/Sameer_cv.pdf">
+                    
                     <motion.button whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)" }}
                         className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
                                      border-purple-400 rounded-xl info-button"     
-                    >
+                    
+                    onClick={handleDownload}>
                         Download CV
-                    </motion.button></a>
+                    </motion.button>
 
                     <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20 info-links">
                         <motion.a whileHover={{ scale: 1.2 }} href="https://github.com/Malik-Sameer" target="blank">
